07df6640df4f635b73ef94bd8b8fc0fed2d11eaf
Hil <hildebrando.escudero@udc.edu>
2020-01-01 02:34:17 -0500

    make it better

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/angular-build'));
 app.get('/*', function(req,res){
 res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
 
 const path = require("path");
 const express = require("express");
@@ -17,4 +18,4 @@ app.get('/*', function(req,res){
 res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
